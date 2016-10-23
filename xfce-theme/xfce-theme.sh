2c0e3c0ad92802497fd5153a17104d7f9a0af64c
Maxime Loliée <maxime@siliadev.com>
2016-10-23 13:29:36 +0200

    refactor: improve user setup with CUSER/CHOME vars

--- xfce-theme/xfce-theme.sh
+++ xfce-theme/xfce-theme.sh
@@ -4,7 +4,8 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-CUSER=${CUSER:-'root'}
+CUSER=${CUSER:-$USER}
+CHOME=${CHOME:-$HOME}
 LACAPITAINE_ICON_THEME_VERSION=${LACAPITAINE_ICON_THEME_VERSION:-'0.3.0'}
 
 export DEBIAN_FRONTEND=noninteractive
@@ -15,33 +16,27 @@ mkdir -p /root/.versions
 # Ensure curl is installed
 hash curl &>/dev/null || apt-get install -y curl
 
-if [[ ${CUSER} == 'root' ]]; then
-  user_home='/root'
-else
-  user_home="/home/${CUSER}"
-fi
-
-if [[ ! -d "${user_home}/.local/share/themes/axe rounded" ]]; then
+if [[ ! -d "${CHOME}/.local/share/themes/axe rounded" ]]; then
   echo "--> install xfce axe theme..."
   curl -L https://dl.opendesktop.org/api/files/download/id/1460767141/73291-axe.tar.gz -o /tmp/xfce-axe.tar.gz
-  mkdir -p "${user_home}/.local/share/themes"
+  mkdir -p "${CHOME}/.local/share/themes"
   pushd "/tmp" &>/dev/null
     tar xvfz xfce-axe.tar.gz
-    mv axe\ rounded "${user_home}/.local/share/themes/"
-    chown -R "${CUSER}." "${user_home}/.local"
+    mv axe\ rounded "${CHOME}/.local/share/themes/"
+    chown -R "${CUSER}." "${CHOME}/.local"
   popd &>/dev/null
 fi
 
 grep -q "^${LACAPITAINE_ICON_THEME_VERSION}" /root/.versions/lacapitaine &>/dev/null || {
-  if [[ ! -d "${user_home}/.icons/la-capitaine-icon-theme-${LACAPITAINE_ICON_THEME_VERSION}" ]]; then
+  if [[ ! -d "${CHOME}/.icons/la-capitaine-icon-theme-${LACAPITAINE_ICON_THEME_VERSION}" ]]; then
     echo "--> install icons theme lacapitaine v${LACAPITAINE_ICON_THEME_VERSION}..."
     curl -L "https://dl.opendesktop.org/api/files/download/id/1472166359/la-capitaine-icon-theme-${LACAPITAINE_ICON_THEME_VERSION}.tar.gz" \
       -o "/tmp/lacapitaine-icon-theme-${LACAPITAINE_ICON_THEME_VERSION}.tar.gz"
-    mkdir -p "${user_home}/.icons"
+    mkdir -p "${CHOME}/.icons"
     pushd "/tmp" &>/dev/null
       tar xvfz "lacapitaine-icon-theme-${LACAPITAINE_ICON_THEME_VERSION}.tar.gz"
-      mv "la-capitaine-icon-theme-${LACAPITAINE_ICON_THEME_VERSION}" "${user_home}/.icons/la-capitaine-icon-theme-${LACAPITAINE_ICON_THEME_VERSION}"
-      chown -R "${CUSER}." "${user_home}/.icons"
+      mv "la-capitaine-icon-theme-${LACAPITAINE_ICON_THEME_VERSION}" "${CHOME}/.icons/la-capitaine-icon-theme-${LACAPITAINE_ICON_THEME_VERSION}"
+      chown -R "${CUSER}." "${CHOME}/.icons"
     popd &>/dev/null
     echo "${LACAPITAINE_ICON_THEME_VERSION}" >/root/.versions/lacapitaine
   fi
